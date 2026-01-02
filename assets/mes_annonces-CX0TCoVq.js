92b6e383c748bf4289fad7b6ef49edeb816e6252
imad <imad@imad.com>
2026-01-02 13:55:38 +0100

    first commit

---
+++ assets/mes_annonces-CX0TCoVq.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-Bw-RzcdV.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/mes_annonces"!'})}export{e as component};
---
+++ assets/route-BQXfV7ZW.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Bw-RzcdV.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
