b8863a684722784c9d4ad84b0a93a332e79d00a9
imad <imad@imad.com>
2026-02-03 23:58:10 +0100

    ;

---
+++ assets/cv-B03Rkp8s.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-dm3QBHmQ.js";function t(){return o.jsx("div",{children:'Hello "/$lang/job/cv"!'})}export{t as component};
---
+++ assets/route-CiOoY6us.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-dm3QBHmQ.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
