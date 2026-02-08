2597964d7be205c62b88c8bdba6f95f15e355cdb
imad <imad@imad.com>
2026-02-08 21:30:49 +0100

    fg

--- assets/index-BzpwaOvA.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CNlayydN.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/"!'})}export{e as component};
---
+++ assets/route-C_hPKocE.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CtvaOUFX.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
