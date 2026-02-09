c0b6de072ece6508795ad9a94901738231dd7073
imad <imad@imad.com>
2026-02-09 02:46:26 +0100

    fg

--- assets/route-C_hPKocE.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CtvaOUFX.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
---
+++ assets/route-CqPyWa58.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DLwvBtN-.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
