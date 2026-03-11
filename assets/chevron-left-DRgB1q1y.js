d80d98c5333722158d4e30778f6512736d2b9089
imad <imad@imad.com>
2026-03-11 10:49:09 +0100

    jj

---
+++ assets/chevron-left-DRgB1q1y.js
@@ -0,0 +1,1 @@
+import{l as e}from"./index-DR2eGgCg.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("chevron-left",o);export{c as C};
---
+++ assets/route-CLL7s9sH.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DR2eGgCg.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
