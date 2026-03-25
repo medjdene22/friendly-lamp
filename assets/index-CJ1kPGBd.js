a96896239ff7ca030088858c8fc86b7034a7bf4d
imad <imad@imad.com>
2026-03-25 18:55:58 +0100

    f

--- assets/index-CJ1kPGBd.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-_-gwKCGd.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/"!'})}export{e as component};
--- assets/route-ChxLaMZQ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-_-gwKCGd.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
