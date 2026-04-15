57a64d522642c88ecf56f5a4375ab5ff71e3b8ee
imad <imad@imad.com>
2026-04-15 02:28:40 +0100

    f

---
+++ assets/chevron-left-C6g2X4ik.js
@@ -0,0 +1,1 @@
+import{m as e}from"./index-CvXv-YZ5.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("chevron-left",o);export{c as C};
--- assets/route-3LVW6RBo.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BBiOMiH5.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
