1f8ba062d7ce854fb224ba3b4f433f67002feb47
imad <imad@imad.com>
2026-04-17 02:58:16 +0100

    dd

---
+++ assets/arrow-right-DMUCDDh2.js
@@ -0,0 +1,1 @@
+import{m as o}from"./index-Ci6t4Zv3.js";const t=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],e=o("arrow-left",t);const r=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],c=o("arrow-right",r);export{e as A,c as a};
---
+++ assets/route-Db9TBfOV.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Ci6t4Zv3.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
