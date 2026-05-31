e813a239cb1ffc38430d524e1b84833647d69e73
imad <imadmedjdene@gmail.com>
2026-05-31 13:12:04 +0100

    chore: commit changes

--- assets/index-DAArkMs4.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-D8WhB_zV.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/"!'})}export{e as component};
---
+++ assets/route-B4TWKym8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CR9YvO-i.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
