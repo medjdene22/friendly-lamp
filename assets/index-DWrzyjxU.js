d2afe28745888e3dd8b47e3f5198a5571f4cda64
imad <imadmedjdene@gmail.com>
2026-06-30 23:54:45 +0100

    gfer

---
+++ assets/index-DWrzyjxU.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-uL5pk2HM.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/"!'})}export{e as component};
---
+++ assets/route-C6HlobIi.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-uL5pk2HM.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
