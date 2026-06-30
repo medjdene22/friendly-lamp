f6fccb243d31f8699427e23e27f226d1135c84e8
imad <imadmedjdene@gmail.com>
2026-06-30 23:56:10 +0100

    wef

---
+++ assets/getWilaya-CDihMFGA.js
@@ -0,0 +1,1 @@
+import{x as t,ak as e}from"./index-BnFihNGz.js";const u=({lang:a="fr_FR"})=>t({staleTime:1/0,queryKey:["wilayas",a],queryFn:async()=>{const{data:s}=await e.get("/api/static/wilayas?lang="+a);return s}});export{u};
