b77bd983255028f392f36fc7886131283424f3c2
imad <imadmedjdene@gmail.com>
2026-07-05 09:38:52 +0100

    ds

---
+++ assets/index-C7cfqGW-.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-ios4vy3V.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/"!'})}export{e as component};
---
+++ assets/route-DtzJmfgJ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-ios4vy3V.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
