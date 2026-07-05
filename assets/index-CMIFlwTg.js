56da2cde33dedb5b251a48ba1f6c92777d9f792d
imad <imadmedjdene@gmail.com>
2026-07-05 10:19:24 +0100

    gf

---
+++ assets/index-CMIFlwTg.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-DSqPi96z.js";function e(){return n.jsx("div",{className:"sm:container",children:'Hello "/$lang/"!'})}export{e as component};
---
+++ assets/route-CxkrGMl4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DSqPi96z.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
