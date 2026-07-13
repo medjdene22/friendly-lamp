21dc4d8fc2f859bfaba92138b10702ee982c2818
imad <imadmedjdene@gmail.com>
2026-07-13 12:41:06 +0100

    ds

---
+++ assets/route-BnEaGLTr.js
@@ -0,0 +1,1 @@
+import{j as o}from"./react-vendor-kecG6-Am.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
--- assets/route-pl68M3ji.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./tanstack-vendor-D96yEMoh.js";const n=()=>o.jsx("div",{children:"no internet"});export{n as errorComponent};
